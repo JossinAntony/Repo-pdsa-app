bce7b508edc04652e93973e13d8c66918081fd31
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-09-21 23:15:49 +0530

    sidebar experiment-draft1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatTableModule, MatCardModule, MatFormFieldModule, MatSelectModule, MatOptionModule} from '@angular/material';
+import {MatTableModule, MatCardModule, MatFormFieldModule, MatSelectModule, MatOptionModule } from '@angular/material';
 import {MatInputModule, MatButtonModule, MatExpansionModule, MatToolbarModule} from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatNativeDateModule, MatSidenavModule, MatListModule} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 import { SurveyComponent } from './survey/survey.component';
@@ -16,11 +18,18 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { LayoutModule } from '@angular/cdk/layout';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
 const appRoutes: Routes = [
   {path: '', component: SurveyComponent},
   {path: 'adminPanel', component: AdminPanelComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+
+
+  { path: 'first', component: FirstComponent, data: { title: 'First Component' } },
+  { path: 'second', component: SecondComponent, data: { title: 'Second Component' } }
 ];
 
 @NgModule({
@@ -29,7 +38,10 @@ const appRoutes: Routes = [
     SurveyComponent,
     AdminPanelComponent,
     NavbarComponent,
-    DashboardComponent
+    DashboardComponent,
+    SidenavComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
@@ -42,7 +54,8 @@ const appRoutes: Routes = [
     MatFormFieldModule,
     ReactiveFormsModule,
     MatInputModule, MatButtonModule, MatExpansionModule, MatToolbarModule,
-    MatSelectModule, MatOptionModule, MatGridListModule, MatMenuModule, MatIconModule, LayoutModule
+    MatSelectModule, MatOptionModule, MatGridListModule, MatMenuModule, MatIconModule, LayoutModule,
+    MatNativeDateModule, MatSidenavModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent, NavbarComponent]
