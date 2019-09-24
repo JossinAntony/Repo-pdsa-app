9ff909fd306525b3240c4522544210817baf9b98
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-09-25 04:13:30 +0530

    compensation calculation data extraction fromperson works.

--- src/app/compensation/compensation.component.ts
+++ src/app/compensation/compensation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-compensation',
@@ -8,13 +9,47 @@ import { NgForm } from '@angular/forms';
 })
 export class CompensationComponent implements OnInit {
 
-  constructor() { }
+  person: any;
+  dCount = 0;
+  iCount = 0;
+  aCount = 0;
+  arrLength = 0;
+
+
+  constructor(private apiservice: ApiService) { }
 
   ngOnInit() {
   }
 
   onSubmit(data: NgForm) {
-    console.log(data.value);
+
+    this.apiservice.retrievePersonByName(data.value).subscribe((response: Array<any>) => {
+      if (response.length > 0) {
+        this.dCount = 0;
+        this.person = response[0];
+
+        for (let i = 0; i < this.person.casualities.length; i++) {
+          if (this.person.casualities[i].casStatus === 'deceased') {
+            this.dCount = this.dCount + 1;
+          }
+          if (this.person.casualities[i].casStatus === 'injured') {
+            this.iCount = this.iCount + 1;
+          }
+        }
+
+        for (let i = 0; i < this.person.assets.length; i++) {
+          if (this.person.assets[i].astStatus === 'total') {
+            this.aCount = this.aCount + 1;
+          }
+
+        }
+        console.log(this.dCount, this.iCount, this.aCount);
+
+      } else {
+        alert('No matching entires found!');
+
+      }
+    });
   }
 
 }
