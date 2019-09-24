15e4f629f11e540b64705ff8dceecaa2a738a902
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-09-25 02:57:03 +0530

    http to https in apiservices.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,18 +9,18 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   savePeople(data) {
-    return this.http.post('http://localhost:3052/savePeopleAPI', data);
+    return this.http.post('https://localhost:3052/savePeopleAPI', data);
   }
 
   retrievePeople() {
-    return this.http.get('http://localhost:3052/retrievePeopleAPI');
+    return this.http.get('https://localhost:3052/retrievePeopleAPI');
   }
 
   retrievePersonByName(data) {
-    return this.http.post('http://localhost:3052/retrievePersonByNameAPI', data);
+    return this.http.post('https://localhost:3052/retrievePersonByNameAPI', data);
   }
 
   deletePersonById(data) {
-    return this.http.post('http://localhost:3052/deletePersonAPI', data);
+    return this.http.post('https://localhost:3052/deletePersonAPI', data);
   }
 }
