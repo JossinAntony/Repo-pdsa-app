fbef35d213f8b5e542a939798466dc30b3672068
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-09-25 03:09:28 +0530

    apiservices link changed from localhost to herokku.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,18 +9,22 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   savePeople(data) {
-    return this.http.post('https://localhost:3052/savePeopleAPI', data);
+    // return this.http.post('http://localhost:3052/savePeopleAPI', data);
+    return this.http.post('https://pdsa-apis.herokuapp.com/savePeopleAPI', data);
   }
 
   retrievePeople() {
-    return this.http.get('https://localhost:3052/retrievePeopleAPI');
+    // return this.http.get('http://localhost:3052/retrievePeopleAPI');
+    return this.http.get('https://pdsa-apis.herokuapp.com/retrievePeopleAPI');
   }
 
   retrievePersonByName(data) {
-    return this.http.post('https://localhost:3052/retrievePersonByNameAPI', data);
+    // return this.http.post('http://localhost:3052/retrievePersonByNameAPI', data);
+    return this.http.post('https://pdsa-apis.herokuapp.com/retrievePersonByNameAPI', data);
   }
 
   deletePersonById(data) {
-    return this.http.post('https://localhost:3052/deletePersonAPI', data);
+    // return this.http.post('http://localhost:3052/deletePersonAPI', data);
+    return this.http.post('https://pdsa-apis.herokuapp.com/deletePersonAPI', data);
   }
 }
